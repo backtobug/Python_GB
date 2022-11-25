ee4f1c86e9a4ff5b98069655ae0dfc0a2b4c5129
Ilya <morozovip@mail.ru>
2022-11-25 19:06:19 +0300

    done all task

--- seminar_2/seminar_2.py
+++ seminar_2/seminar_2.py
@@ -5,3 +5,30 @@ for i in num:
         continue
     count += int(i)
 print(count)
+# ==============================================
+#
+# num = int(input('Введите число: '))
+# temp = 1
+# lst =[]
+#
+# for i in range(1, num+1):
+#     temp = temp * i
+#     lst.append(temp)
+# print(lst)
+# =================================================
+
+# num = int(input('Введите число: '))
+# list = []
+# for i in range (1, num+1):
+#     list.append((1+1/i)**i)
+# print(list)
+# print(sum(list))
+# =================================================
+#
+# import random
+# num = int(input('Введите размер списка: '))
+# lst = [random.randint(0,100) for i in range(num)]
+# print(f"список:\n{lst}")
+# random.shuffle(lst)
+# print(f"перемешанный список:\n{lst}")
+# ===================================================

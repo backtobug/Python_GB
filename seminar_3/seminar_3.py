9341bf7355d1740a9ad0481132ef57de5d727e32
Ilya <morozovip@mail.ru>
2022-11-28 23:13:57 +0300

    done

--- seminar_3/seminar_3.py
+++ seminar_3/seminar_3.py
@@ -1,12 +1,31 @@
+# from random import randint
+#
+# lst = [randint(1, 10) for i in range(10)]
+#
+# def odd_position(a):
+#     count = 0
+#     for i in range(0, len(a)):
+#         if i % 2 == 1:
+#             count += a[i]
+#     print(f'{lst} ==> {count}')
+#
+# odd_position(lst)
+# ===========================================
+
 from random import randint
 
-lst = [randint(1, 10) for i in range(10)]
+lst = [randint(1, 10) for i in range(5)]
 
-def odd_position(a):
-    count = 0
-    for i in range(0, len(a)):
-        if i % 2 == 1:
-            count += a[i]
-    print(f'{lst} ==> {count}')
+def multiply_pairs(a):
+    rev = a[::-1]
+    count = round(len(a) / 2)
+    result = []
+    if len(a) % 2 == 0:
+        for i in range(0, count):
+            result.append(lst[i] * rev[i])
+    else:
+        for i in range(0, count + 1):
+            result.append(lst[i] * rev[i])
+    print(f'{lst} ==> {result}')
 
-odd_position(lst)
+multiply_pairs(lst)

4ab810a111def15730531613f5bca86e855a06fa
Ilya <morozovip@mail.ru>
2022-12-04 03:12:35 +0300

    done easy

--- seminar_4/seminar_4.py
+++ seminar_4/seminar_4.py
@@ -12,18 +12,28 @@
 # print(f'Число {pi:.{count}f}')
 # =====================================================
 
-def simple_f(n):
-    i = 2
-    lst = []
-    while i * i <= n:
-        while n % i == 0:
-            lst.append(i)
-            n = n / i
-        i = i + 1
-    if n > 1:
-        lst.append(n)
-    print(lst)
+# def simple_f(n):
+#     i = 2
+#     lst = []
+#     while i * i <= n:
+#         while n % i == 0:
+#             lst.append(i)
+#             n = n / i
+#         i = i + 1
+#     if n > 1:
+#         lst.append(n)
+#     print(lst)
+#
+#
+# simple_f(100)
+# =========================================================
 
+num = 1113384455229567467970
+st = str(num)
+lst = []
+for i in st:
+    if st.count(i) < 2:
+        lst.append(int(i))
+print(lst)
 
-simple_f(100)
 

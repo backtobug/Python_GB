876778fea0aca7f1650bbf23b04555858e1efc9e
Ilya <morozovip@mail.ru>
2022-12-04 03:31:24 +0300

    done easy

--- seminar_4/seminar_4.py
+++ seminar_4/seminar_4.py
@@ -28,12 +28,38 @@
 # simple_f(100)
 # =========================================================
 
-num = 1113384455229567467970
-st = str(num)
-lst = []
-for i in st:
-    if st.count(i) < 2:
-        lst.append(int(i))
-print(lst)
+# num = 1113384455229567467970
+# st = str(num)
+# lst = []
+# for i in st:
+#     if st.count(i) < 2:
+#         lst.append(int(i))
+# print(lst)
+# ===========================================================
 
-
+# from random import randint
+# import random
+#
+# num = int(input('Введите число '))
+#
+# res = ''
+# temp = []
+# for i in range(num):
+#     temp.append(randint(0, 100))
+#
+# sym = ['+', '-']
+# i = 0
+# j = 0
+# while num > 1:
+#     if temp[i] != 0:
+#         res += f'{temp[i]}x**{num}{random.choice(sym)}'
+#     num -= 1
+#     i += 1
+#
+# if temp[-1] != 0:
+#     res += f'{temp[-1]}=0'
+# else:
+#     res += '=0'
+# with open('res.txt', 'a+', encoding='utf8' ) as file:
+#     file.write(f'{temp} \n {res}')
+# ===============================================================

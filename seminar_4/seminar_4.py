5607050ee061936aa47bc85380bde3e0826c0219
Ilya <morozovip@mail.ru>
2022-12-04 03:54:29 +0300

    done

--- seminar_4/seminar_4.py
+++ seminar_4/seminar_4.py
@@ -1,15 +1,9 @@
-# import math
-#
-# pi = math.pi
-# num = input('Введите число d пример: 0.001, 0,01: ')
-# count = 0
-#
-# num = num.replace('0.', '')
-#
-# for i in num:
-#     count += 1
-#
-# print(f'Число {pi:.{count}f}')
+import math
+
+pi = math.pi
+num = input('Введите число: 0.001, 0.01 и тд..: ')
+count = num.count('0')
+print(f'Число {pi:.{count}f}')
 # =====================================================
 
 # def simple_f(n):

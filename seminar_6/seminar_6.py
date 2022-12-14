a179051a91fa3547eb3fc701edbda8a32bda5897
Ilya <morozovip@mail.ru>
2022-12-14 04:31:05 +0300

    done

--- seminar_6/seminar_6.py
+++ seminar_6/seminar_6.py
@@ -3,20 +3,19 @@ lst = [1, 4, 5, 2, 3, 9, 8, 11, 0]
 lst.sort()
 lst2 = [[lst[0]]]
 j = lst[0]
-print (lst)
 
 for i in lst[1:]:
-    if i-j == 1:
-            lst2[-1].append(i)
+    if i - j == 1:
+        lst2[-1].append(i)
     else:
-            lst2.append([i])
+        lst2.append([i])
     j = i
-rez1 = [i[0] for i in lst2]
-rez2 = [i[len(i)-1] for i in lst2]
-print (lst2)
-print (str(rez1[0])+'-'+str(rez2[0]))
+result = [i[0] for i in lst2]
+result2 = [i[len(i) - 1] for i in lst2]
+print(lst2)
+print(str(result[0]) + '-' + str(result2[0]))
 
-k=0
-while k < len(rez1):
-    print (str(rez1[k])+'-'+str(rez2[k]))
+k = 0
+while k < len(result):
+    print(str(result[k]) + '-' + str(result2[k]))
     k += 1
